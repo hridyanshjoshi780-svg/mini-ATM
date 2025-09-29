e52739599c69076db0fcc5b37b19f2a0bed7efc3
Hridyansh joshi <hridyanshjoshi780@gmail.com>
2025-09-29 22:30:29 +0530

    Update miniATM.cpp

--- miniATM.cpp
+++ miniATM.cpp
@@ -1,6 +1,5 @@
 #include<iostream>
 using namespace std;
-
 float takeout(float account, float withdraw) {
     if (account >= withdraw) {
         account -= withdraw;
@@ -11,49 +10,38 @@ float takeout(float account, float withdraw) {
     }
     return account;
 }
-
 float moneyin(float account, float deposit) {
     account += deposit;
     cout << "Amount deposited successfully." << endl;
     cout << "Current account balance: " << account << endl;
     return account;
 }
-
 int main() {
     float account = 10000.0;
     float deposit, withdraw;
     char check;
-
     do {
         cout << "\nEnter 'w' to withdraw, 'd' to deposit, 'c' to check balance, or 'e' to exit: ";
         cin >> check;
-
         switch (check) {
             case 'w':
                 cout << "Enter amount to withdraw: ";
                 cin >> withdraw;
                 account = takeout(account, withdraw);
                 break;
-
             case 'd':
                 cout << "Enter amount to deposit: ";
                 cin >> deposit;
                 account = moneyin(account, deposit);
                 break;
-
             case 'c':
                 cout << "Your account balance is: " << account << endl;
                 break;
-
             case 'e':
                 cout << "Exiting the system. Goodbye!" << endl;
                 break;
-
             default:
                 cout << "Invalid input. Please try again." << endl;
-        }
-
     } while (check != 'e');
-
     return 0;
 }
