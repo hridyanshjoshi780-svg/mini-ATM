b35613472e6df768956c5a48545da7939847fc5d
Hridyansh joshi <hridyanshjoshi780@gmail.com>
2025-09-29 22:27:16 +0530

    Update miniATM.cpp

--- miniATM.cpp
+++ miniATM.cpp
@@ -1,39 +1,59 @@
 #include<iostream>
 using namespace std;
-float takeout(float account , float withdraw){
-if (account >= withdraw ){
-    cout<<"money withdraw = "<<withdraw<<endl;
-}else if (account < withdraw ){
-    cout<<"your account don't have sufficient amount to take out please deposit first."<<endl;
+
+float takeout(float account, float withdraw) {
+    if (account >= withdraw) {
+        account -= withdraw;
+        cout << "Withdrawal complete. Amount withdrawn: " << withdraw << endl;
+        cout << "Amount left in account: " << account << endl;
+    } else {
+        cout << "Insufficient funds. Please deposit more money." << endl;
+    }
+    return account;
 }
-cout <<"amount left in account "; return account - withdraw ;
+
+float moneyin(float account, float deposit) {
+    account += deposit;
+    cout << "Amount deposited successfully." << endl;
+    cout << "Current account balance: " << account << endl;
+    return account;
 }
-float moneyin(float account , float deposit){
-    cout<<"your entered ammount is added to your ammount.";
-    cout<<"now your account has"; return account + deposit;
-}
-int main(){
-float account = 10000 ;
-float deposit ;
-float withdraw ;
-char check ;
-cout<<"enter withdraw (w) or deposit(d) or check amount (c) for respective action : ";
-cin>>check;
-while (check != 'e')
-{
-    if(check == 'w'){
-    cout<<"how much you want to withdraw : ";
-cin>>withdraw;
-    cout<< takeout(account , withdraw);
-}else if (check == 'd'){
-    cout<<"how much you want to deposit : ";
-    cin>>deposit;
-    cout<< moneyin(account , deposit);
-}else if (check = 'c'){
-    cout<<"your account has "<< account << " amount" << endl;
-}else{
-    cout<<"Invalid input";
-}
-}
-    return 0 ;
+
+int main() {
+    float account = 10000.0;
+    float deposit, withdraw;
+    char check;
+
+    do {
+        cout << "\nEnter 'w' to withdraw, 'd' to deposit, 'c' to check balance, or 'e' to exit: ";
+        cin >> check;
+
+        switch (check) {
+            case 'w':
+                cout << "Enter amount to withdraw: ";
+                cin >> withdraw;
+                account = takeout(account, withdraw);
+                break;
+
+            case 'd':
+                cout << "Enter amount to deposit: ";
+                cin >> deposit;
+                account = moneyin(account, deposit);
+                break;
+
+            case 'c':
+                cout << "Your account balance is: " << account << endl;
+                break;
+
+            case 'e':
+                cout << "Exiting the system. Goodbye!" << endl;
+                break;
+
+            default:
+                cout << "Invalid input. Please try again." << endl;
+        }
+
+    } while (check != 'e');
+
+    return 0;
 }
